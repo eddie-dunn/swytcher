f67e9a017d063586f5c1ad7f42e6060687158c0e
Eddie Dunn <eddie.dunn@gmail.com>
2017-03-27 00:00:42 +0200

    add log conf file

--- swytcher/cli.py
+++ swytcher/cli.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """CLI frontend for Swytcher"""
+import logging
+import logging.config
+import os
 
 import click
 
@@ -13,6 +16,11 @@ def main(args=None):
                "swytcher.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
     print(args or "NO ARGS GIVEN")
+    # Setup logging
+    logfile = "{}{}{}".format(
+        os.path.dirname(__file__), os.path.sep, 'log_conf.ini')
+    logging.config.fileConfig(logfile, disable_existing_loggers=False)
+
     swytcher.main(args)
 
 
