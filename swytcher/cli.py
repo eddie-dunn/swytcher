26ae825c6cf9723edacba11a9d9d315eb3c508c0
Edward Ekelund <edwardd@axis.com>
2017-03-30 00:13:05 +0200

    cli.py: Get log config file from confdirs

--- swytcher/cli.py
+++ swytcher/cli.py
@@ -9,14 +9,34 @@ import click
 import swytcher.swytcher as swytcher
 
 
+def get_config(filename: str) -> str:
+    """Try to find user configured logfile"""
+    # NOTE: Move to settings later
+    logfile = ''
+    home = os.path.expanduser('~')
+    paths = [
+        '{}/.config/swytcher/{}'.format(home, filename),
+        '{}/.local/swytcher/config/{}'.format(home, filename),
+    ]
+    for path in paths:
+        if os.path.isfile(path):
+            logfile = path
+            break
+    return logfile
+
+
 @click.command()
 def main(args=None):
     """Console script for swytcher"""
     # click.echo("See click documentation at http://click.pocoo.org/")
     click.echo(__name__)
     # Setup logging
-    logfile = "{}{}{}".format(
-        os.path.dirname(__file__), os.path.sep, 'log_conf.ini')
+    logfile = get_config('log_conf.ini')
+
+    if not logfile:
+        logfile = '{}/log_conf.ini'.format(os.path.dirname(__file__))
+        print("No log conf file found, using default %r" % logfile)
+
     logging.config.fileConfig(logfile, disable_existing_loggers=False)
 
     swytcher.main(args)
