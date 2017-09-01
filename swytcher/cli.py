3e4d4ed704ae270eb31c09cc847c4ec239004caa
Eddie Dunn <eddie.dunn@gmail.com>
2017-09-01 12:47:43 +0200

    cli.py: Add initial logconfig

--- swytcher/cli.py
+++ swytcher/cli.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 """CLI frontend for Swytcher"""
+import logging
+
 import click
 
 import swytcher.settings as settings
 import swytcher.swytcher as swytcher
+
+log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+logging.basicConfig(level=logging.INFO)
 
 
 @click.command()
 def main(args=None):
     """Console script for swytcher"""
     # click.echo("See click documentation at http://click.pocoo.org/")
-    click.echo(__name__)
+    # click.echo(__name__)
     settings.load_configs()  # pylint: disable=protected-access
 
     swytcher.main(args)
