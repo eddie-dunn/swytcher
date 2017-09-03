5c7f4ddbf8c010cffb1cf0a24a0867b2b6963f9e
Eddie Dunn <eddie.dunn@gmail.com>
2017-09-03 05:13:22 +0200

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='swytcher',
-    version='0.3.0',
+    version='0.3.1',
     description="Automatically switch layout based on your active window",
     long_description=readme + '\n\n' + history,
     author="Eddie Dunn",
--- swytcher/__init__.py
+++ swytcher/__init__.py
@@ -2,4 +2,4 @@
 # pylint: skip-file
 __author__ = """Eddie Dunn"""
 __email__ = 'eddie.dunn@gmail.com'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
