2f4a1508f5f55120abc137a46fc4544c03747eea
Eddie Dunn <eddie.dunn@gmail.com>
2017-11-20 02:47:40 +0100

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='swytcher',
-    version='0.3.1',
+    version='0.3.2',
     description="Automatically switch layout based on your active window",
     long_description=readme + '\n\n' + history,
     author="Eddie Dunn",
